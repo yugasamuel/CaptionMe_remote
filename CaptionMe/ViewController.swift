c3a411681c2499f7e935b7e8f5e2783fb38c13fa
Yuga Samuel <yugasamuel2002@gmail.com>
2023-09-15 20:59:34 +0700

    Add table view cell

--- CaptionMe/ViewController.swift
+++ CaptionMe/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class ViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     var photos = [Photo]()
     
     override func viewDidLoad() {
@@ -21,19 +21,56 @@ class ViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Photo", for: indexPath)
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Photo", for: indexPath) as? PhotoCell else {
+            fatalError("Unable to dequeue Photo")
+        }
+        
+        let photo = photos[indexPath.item]
         
         var content = cell.defaultContentConfiguration()
-        content.text = photos[indexPath.row].caption
+        content.text = photo.caption
+        
+        let path = getDocumentsDirectory().appending(path: photo.image)
+        content.image = UIImage(contentsOfFile: path.path())
+        
         cell.contentConfiguration = content
         
         return cell
     }
     
     @objc func addPhoto() {
-        photos.append(Photo(caption: "dummy", image: "dummy"))
-        tableView.reloadData()
+        let picker = UIImagePickerController()
+        /*
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        }
+        */
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
     }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+
+        let imageName = UUID().uuidString
+        let imagePath = getDocumentsDirectory().appendingPathComponent(imageName)
+
+        if let jpegData = image.jpegData(compressionQuality: 0.8) {
+            try? jpegData.write(to: imagePath)
+        }
+        
+        let photo = Photo(caption: "N/A", image: imageName)
+        photos.append(photo)
+        tableView.reloadData()
+        
+//        save()
+        dismiss(animated: true)
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
 }
 
