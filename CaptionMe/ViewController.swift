9b36f441422405a904421e2e8569e7ea31998636
Yuga Samuel <yugasamuel2002@gmail.com>
2023-09-15 19:34:37 +0700

    Create table view functions in ViewController

--- CaptionMe/ViewController.swift
+++ CaptionMe/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UITableViewController {
-
+    var photos = [Photo]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addPhoto))
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Photo", for: indexPath)
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = photos[indexPath.row].caption
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+    
+    @objc func addPhoto() {
+        photos.append(Photo(caption: "dummy", image: "dummy"))
+        tableView.reloadData()
+    }
+    
 }
 
